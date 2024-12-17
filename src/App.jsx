3c381f8da8a5539e52830ccd8dddf8c45c012fb4
Marat Frost <morozovmarat4@gmail.com>
2024-12-18 01:30:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import image from './images/image copy.png';
 
 
 function App() {
   return (
     <>
       <header>
-      <img className="photo" src="/public/images/image copy.png" alt="Profile" />
+      <img className="photo" src={image} alt="Profile" />
         <h1 style={{ fontSize: '340%' }}>MARAT MOROZOV</h1>
         <h2>Python Developer</h2>
         <p className="about">
